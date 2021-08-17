73940add0715c37a1d9b872ae8d683575646d876
Fiona Natasha Valentio <fio.nat.valen@gmail.com>
2021-08-17 16:23:04 +0700

    Fixed postgreql string 2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wpaulpdizphrcp:88620d9b4b409216aabc69dd7efe8649b4a4b605bcc920f0655bfda9b12a65c7@ec2-44-196-44-90.compute-1.amazonaws.com:5432/dc2gqv0ds4ud74'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wpaulpdizphrcp:88620d9b4b409216aabc69dd7efe8649b4a4b605bcc920f0655bfda9b12a65c7@ec2-44-196-44-90.compute-1.amazonaws.com:5432/dc2gqv0ds4ud74'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'fiona'
 api = Api(app)
