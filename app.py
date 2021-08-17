d31e37f4c4de1640db7a36f6daca6fb39811f1c8
Fiona Natasha Valentio <fio.nat.valen@gmail.com>
2021-08-17 16:20:09 +0700

    Fixed postgreql string

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wpaulpdizphrcp:88620d9b4b409216aabc69dd7efe8649b4a4b605bcc920f0655bfda9b12a65c7@ec2-44-196-44-90.compute-1.amazonaws.com:5432/dc2gqv0ds4ud74'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'fiona'
 api = Api(app)
